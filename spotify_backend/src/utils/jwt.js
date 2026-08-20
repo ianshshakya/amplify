483a582ac3c2636968a88e3581796d46825a8622
Ansh Shakya <anshshakya0712@gmail.com>
2026-08-21 00:33:40 +0530

    Fix login/signup 500 error by adding JWT_SECRET fallback for Render

--- spotify_backend/src/utils/jwt.js
+++ spotify_backend/src/utils/jwt.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 function signToken(userId) {
-  return jwt.sign({ sub: userId }, process.env.JWT_SECRET, {
+  const secret = process.env.JWT_SECRET || 'fallback_secret_do_not_use_in_prod';
+  return jwt.sign({ sub: userId }, secret, {
     expiresIn: process.env.JWT_EXPIRES_IN || '7d',
   });
 }
